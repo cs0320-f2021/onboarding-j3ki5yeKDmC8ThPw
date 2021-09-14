1c74d92be172d444865f3a18764e2f01eb1d278d
Nick <nicholas_sawicki@brown.edu>
2021-09-14 01:39:57 -0400

    completed part 5

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -67,9 +67,17 @@ public final class Main {
         try {
           input = input.trim();
           String[] arguments = input.split(" ");
-          System.out.println(arguments[0]);
+          MathBot mathBot = new MathBot();
+          // System.out.println(arguments[0]);
           // TODO: complete your REPL by adding commands for addition "add" and subtraction
           //  "subtract"
+          if(arguments[0].equals("add")) {
+            System.out.println(mathBot.add(Double.parseDouble(arguments[2]),Double.parseDouble(arguments[1])));
+          } else if(arguments[0].equals("subtract")) {
+            System.out.println(mathBot.subtract(Double.parseDouble(arguments[2]),Double.parseDouble(arguments[1])));
+          } else {
+            System.out.println("invalid entries");
+          }
         } catch (Exception e) {
           // e.printStackTrace();
           System.out.println("ERROR: We couldn't process your input");
